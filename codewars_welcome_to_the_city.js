7cd3f97568e04c71510002fcf7b66ffa1d819683
Crew Spence <csspence123@gmail.com>
2021-01-23 07:25:32 -0600

    Wrote working solution.

--- codewars_welcome_to_the_city.js
+++ codewars_welcome_to_the_city.js
@@ -7,5 +7,6 @@ sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
 This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
 */
 
-function sayHello( name, city, state ) {
+const sayHello = ( name, city, state ) => {
+  return 'Hello, ' + name.join(' ') + '! ' + 'Welcome to ' + city + ', ' + state + '!';
 }
